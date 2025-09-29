726557ce63fa64b265c538345392ab4eaba1f5b5
Musa Şahan <146845215+msshn01@users.noreply.github.com>
2025-09-29 17:39:19 +0300

    new

--- app/src/main/java/com/example/appuser/activity/MainActivity.java
+++ app/src/main/java/com/example/appuser/activity/MainActivity.java
@@ -324,7 +324,7 @@ public class MainActivity extends AppCompatActivity  {
             startActivity(intent);
             finish();
         }else {
-            
+            //commit
         }
 
 
